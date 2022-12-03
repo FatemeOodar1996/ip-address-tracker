b31a425326a9ed4ca880c11f2b7fbcb89e4eed7c
Fateme Oodar <105479013+FatemeOodar1996@users.noreply.github.com>
2022-12-03 10:11:40 +0330

    import map leaflet react

--- ip-tracker/src/App.js
+++ ip-tracker/src/App.js
@@ -1,19 +1,49 @@
-import "./App.css";
 import "./assets/css/style.scss";
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import axios from "axios";
 
 function App() {
+    const apiKey = "at_78FP5IyukK4UMaR1egRhQkPHi6uPf";
+    const apiUrl = "https://geo.ipify.org/api/v1?apiKey=at_78FP5IyukK4UMaR1egRhQkPHi6uPf&ipAddress=8.8.8.8";
+    const [newIp, setNewIp] = useState();
+
+    useEffect(() => {
+        axios
+            .get("https://geo.ipify.org/api/v1?apiKey=at_78FP5IyukK4UMaR1egRhQkPHi6uPf&ipAddress=8.8.8.8")
+            .then(({ data: newIp }) => {
+                setNewIp(newIp);
+            });
+    }, []);
+    // newIp.map((item) => {
+    //     console.log(item.ip);
+    // });
+
+    // console.log(newIp);
+
     return (
         <div className="main">
             <div className="header">
                 <h1>IP Address Tracker</h1>
-                <div className="search-box"><input type="search" name="ip" id="ip" />
-                <button type="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
-                        <path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6" />
-                    </svg>
-                </button></div>
+                <div className="search-box">
+                    <input type="search" name="ip" id="ip" />
+                    <button type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
+                            <path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6" />
+                        </svg>
+                    </button>
+                </div>
             </div>
-            <div className="map">soll</div>
+            <div className="map">
+                <MapContainer center={[51.505, -0.09]} zoom={12} scrollWheelZoom={true}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    <Marker position={[51.505, -0.09]} />
+                </MapContainer>
+            </div>
         </div>
     );
 }
